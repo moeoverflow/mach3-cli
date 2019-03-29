b6c27a04411624308b1d9b876617c82398cf3525
Calvin Xu <1020806611@qq.com>
2019-03-28 23:58:38 -0700

    player setting

--- mach3/mkv_interface.py
+++ mach3/mkv_interface.py
@@ -7,14 +7,14 @@ def mkv_play(fileName, start, end):
     vid_name = sub_name[:-4]
 
     if os.path.exists(vid_name + ".mkv"):
-        command = "mpv --window-scale=0.5 --pause --keep-open=yes --start={} --end={} --sub-file={} {}"\
-        .format(quote(start), quote(end), quote(sub_name), quote(vid_name + ".mkv"))
+        command = "mpv --window-scale=0.5 --pause --keep-open=yes --start={} --sub-file={} {}"\
+        .format(quote(start), quote(sub_name), quote(vid_name + ".mkv"))
     elif os.path.exists(vid_name + ".mp4"):
-        command = "mpv --window-scale=0.5 --pause --keep-open=yes --start={} --end={} --sub-file={} {}"\
-        .format(quote(start), quote(end), quote(sub_name), quote(vid_name + ".mp4"))
+        command = "mpv --window-scale=0.5 --pause --keep-open=yes --start={} --sub-file={} {}"\
+        .format(quote(start), quote(sub_name), quote(vid_name + ".mp4"))
     elif os.path.exists(vid_name + ".mov"):
-        command = "mpv --window-scale=0.5 --pause --keep-open=yes --start={} --end={} --sub-file={} {}"\
-        .format(quote(start), quote(end), quote(sub_name), quote(vid_name + ".mov"))
+        command = "mpv --window-scale=0.5 --pause --keep-open=yes --start={}--sub-file={} {}"\
+        .format(quote(start), quote(sub_name), quote(vid_name + ".mov"))
     else:
         print("I cannot find the matching video file for you, Misaka tries and fails. \nPlease make sure they have the same file name.")
         exit(1)
